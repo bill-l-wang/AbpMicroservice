ec1f5a9a49587ddd7035ac0e817bbfe2dddbb41a
WANGLEI\wlkux <wlkux@hotmail.com>
2024-07-19 14:47:56 +0800

    add aspire projects

---
+++ src/aspire/AppHost/Program.cs
@@ -0,0 +1,29 @@
+using Macro.AppHost;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddForwardedHeaders();
+
+const string profile = "http";
+
+// Microservices
+var administrationService =
+    builder.AddProject<Projects.Macro_Administration_HttpApi_Host>("administrationService", profile);
+var identityService = builder.AddProject<Projects.Macro_IdentityService_HttpApi_Host>("identityService", profile);
+
+var saasService = builder.AddProject<Projects.Macro_SaaS_HttpApi_Host>("saasService", profile);
+var projectService = builder.AddProject<Projects.Macro_Projects_HttpApi_Host>("cmsKitService", profile);
+
+// Gateways
+var webPublicGateway = builder.AddProject<Projects.Macro_Gateway>("webPublicGateway")
+    .WithReference(administrationService)
+    .WithReference(identityService)
+    .WithReference(saasService)
+    .WithReference(projectService);
+
+// Apps
+var publicWebApp = builder.AddProject<Projects.Macro_Blazor>("public-web", "https")
+    .WithExternalHttpEndpoints()
+    .WithReference(webPublicGateway);
+
+builder.Build().Run();
